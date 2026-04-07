8716626bfe712c0607b59234b74a3af3fb6e0466
Rachidi Sami Ibrahim <rachidi.brahim.sami@gmail.com>
2026-04-07 01:00:08 +0100

    test_azure

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ fs.writeFileSync(FILE, JSON.stringify({ count: 0 }));
 const data = fs.readFileSync(FILE);
 return JSON.parse(data).count;
 } catch (err) {
-console.error("Erreur lecture JSON:", err);
+console.error("Erreur lecture JSON :", err);
 return 0;
 }
 }
